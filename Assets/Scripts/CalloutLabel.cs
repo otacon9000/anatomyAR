cfcfefebf0b0938178acdeb41888a64f4f35f0e0
Andrea Alicino <andrea.alicino@gmail.com>
2021-10-12 11:42:27 +0200

    added all animation status

---
+++ Assets/Scripts/CalloutLabel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CalloutLabel : MonoBehaviour
+{
+    public LineRenderer line;
+    public GameObject target;
+
+
+    private void Start()
+    {
+        line.startWidth = 0.01f;
+        line.endWidth = 0.01f;
+    }
+
+    private void Update()
+    {
+            line.SetPosition(0, this.transform.position);
+            line.SetPosition(1, target.transform.position);
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    public static UIManager Instance { 
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.LogError("UIManager is NULL");
+            }
+            return _instance;
+        } 
+    }
+
+    public Animator anim;
+    private int _currentAnim;
+    public int CurrentAnim 
+    {
+        get
+        {
+            return _currentAnim;
+        }
+        set
+        {
+            _currentAnim = value;
+            if (_currentAnim == 1)
+                label.SetActive(true);
+            else
+                label.SetActive(false);
+        }
+    }
+    public GameObject label;
+
+    private void Awake()
+    {
+        _instance = this;
+        label.SetActive(false);
+    }
+
+    public void Next()
+    {
+        if (CurrentAnim >= 2) return;
+        CurrentAnim++;
+        anim.SetInteger("NextAnim", CurrentAnim);
+    }
+
+    public void Previous()
+    {
+        if (CurrentAnim <= 0) return;
+
+        CurrentAnim--;
+        anim.SetInteger("NextAnim", CurrentAnim);
+    }
+
+
+}
